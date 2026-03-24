29b061e77133e3896013481d8642b4e813572170
Marcel Jacobse <mjacobse@uni-bremen.de>
2026-03-24 21:26:43 +0100

    More defensive anonymous namespace escaping
    For example to avoid adding double escaping if it is already escaped

--- src/debugger.ts
+++ src/debugger.ts
@@ -205,7 +205,7 @@ export class Debugger {
     async unrollTypeAlias(type: string): Promise<string> {
         const debuggerName = this.sessionInfo?.debugger;
         if (debuggerName === 'gdb') {
-            const typeEscaped = type.replace('(anonymous namespace)', "'(anonymous namespace)'");
+            const typeEscaped = type.replace('(anonymous namespace)::', "'(anonymous namespace)'::");
             const evalResult = (await this.evaluate('-exec ptype /rmt ' + typeEscaped))?.result;
             let typeInfo: string = typeof evalResult === 'string' ? evalResult.trim() : '';
             if (typeInfo.startsWith('type = ')) {
